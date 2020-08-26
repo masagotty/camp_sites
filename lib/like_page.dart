695968ca333192677d32b8cd4a55e62d33b6000f
KentaMasago <masagotty@gmail.com>
2020-08-27 08:15:48 +0900

    [WIP] unexpected behavior in LikePage

--- lib/like_page.dart
+++ lib/like_page.dart
@@ -1,14 +1,50 @@
+import 'package:camp_sites/Site.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:camp_sites/site_cell.dart';
+import 'package:camp_sites/site_data.dart';
 
 class LikePage extends StatelessWidget {
-  static const String id = '/user';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: Text(
-        'Like Page',
-      )),
-    );
+    return Consumer<SiteData>(builder: (context, siteData, child) {
+      // List<Site>にしようとするとエラーになる
+      // The argument type 'Site
+      // (where Site is defined in /Users/Gotty/AndroidStudioProjects/MyApps/camp_sites/lib/site.dart)'
+      // can't be assigned to the parameter type 'Site
+      // (where Site is defined in /Users/Gotty/AndroidStudioProjects/MyApps/camp_sites/lib/Site.dart)'
+      List likeSites = [];
+      for (var site in siteData.getSites) {
+        if (site.isLiked = true) {
+          likeSites.add(site);
+          print(likeSites);
+        }
+      }
+      return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          childAspectRatio: 1,
+          crossAxisCount: 2,
+          // crossAxisSpacing: 8,
+          // mainAxisSpacing: 8,
+        ),
+        itemCount: likeSites.length,
+        // padding: EdgeInsets.all(8),
+        itemBuilder: (context, index) {
+          final likeSite = likeSites[index];
+          return SiteCell(
+            siteName: likeSite.name,
+            siteDescription: likeSite.description,
+            siteImageUrl: likeSite.imageUrl,
+            siteLikes: likeSite.likes,
+            siteRate: likeSite.rate,
+            sitePrefecture: likeSite.prefecture,
+            isFavorite: likeSite.isLiked,
+            toggleFavorite: () {
+              siteData.likeSite(likeSite);
+            },
+          );
+        },
+      );
+    });
   }
 }
